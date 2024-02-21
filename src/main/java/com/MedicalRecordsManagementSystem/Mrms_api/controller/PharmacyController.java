9234a768f0f962537e99dc8d31f64e0e4cb728a0
727722euit080@skcet.ac.in <727722euit080@skcet.ac.in>
2024-02-21 10:35:56 +0530

    pharmacy completed

--- src/main/java/com/MedicalRecordsManagementSystem/Mrms_api/controller/PharmacyController.java
+++ src/main/java/com/MedicalRecordsManagementSystem/Mrms_api/controller/PharmacyController.java
@@ -12,8 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -25,14 +28,40 @@ public class PharmacyController {
         this.pharmacyService = pharmacyService;
     }
     @PostMapping("/api/post")
-    public ResponseEntity<PharmacyModel> saveDetails(@RequestBody PharmacyModel pharmacyModel)
+    public ResponseEntity<PharmacyModel> saveMedicine(@RequestBody PharmacyModel pharmacyModel)
     {
         PharmacyModel p= pharmacyService.saveMedicine(pharmacyModel);
         return new ResponseEntity<>(p,HttpStatus.CREATED);
     }
     @GetMapping("/api/getMedicines")
-    public ResponseEntity<List<PharmacyModel>> getMethodName(@RequestParam String param) {
-        return new SomeData();
+    public ResponseEntity<List<PharmacyModel>> getMedicines() {
+        List<PharmacyModel> list=pharmacyService.getAllMedicines();
+        return new ResponseEntity<>(list,HttpStatus.OK);
     }
-    
+    @GetMapping("/api/getMedicine{id}")
+    public  ResponseEntity<PharmacyModel> getById(@PathVariable int id) {
+        PharmacyModel p=pharmacyService.getById(id);
+        return new ResponseEntity<>(p,HttpStatus.OK);
+    }
+    @PutMapping("/put/{id}")
+    public ResponseEntity<PharmacyModel> putMedicine(@PathVariable int id, @RequestBody PharmacyModel pharmacyModel) {
+        if(pharmacyService.updateMedicine(id, pharmacyModel))
+        {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<PharmacyModel> deleteMedicine(@PathVariable int id)
+    {
+        if(pharmacyService.deleteMedicineById(id))
+        {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
 }
